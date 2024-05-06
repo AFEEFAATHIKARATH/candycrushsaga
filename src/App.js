8dcffbd82aae60820773633fd48ecdde15592e13
AFEEFAATHIKARATH <afeefaathikarath@gmail.com>
2024-05-06 21:33:17 +0530

    first changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Game from "./Game";
+import Home from "./Home";
+import Score from "./Score";
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/Game" element={<Game></Game>}></Route>
+        <Route path="/Score" element={<Score></Score>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
